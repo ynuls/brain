a9a9fa28a6437b319adc9ce4e171a045cdc758a9
kn134133 <46958161+kn134133@users.noreply.github.com>
2023-10-09 12:27:55 +0800

    Update MainNav.tsx

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -44,7 +44,7 @@ export function MainNav({ items }: MainNavProps) {
         className="-ml-4 text-base hover:bg-transparent focus:ring-0 md:hidden"
       >
         <Icons.logo className="mr-2 h-4 w-4" />{" "}
-        <span className="font-bold">百谱学科名人图谱</span>
+        <span className="font-bold">数学知识图谱系统</span>
       </Button>
     </div>
   );
