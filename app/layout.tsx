931e4d2cc20d04148fffa133c1760a2bef06419d
kn134133 <46958161+kn134133@users.noreply.github.com>
2023-10-12 20:24:40 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 import { SiteHeader } from "@/components/SiteHeader";
 
 export const metadata = {
-  title: "数学知识图谱系统",
+  title: "Mind-思维导图",
   description:
     "Draw flowchart, sequence diagram, class diagram, user journey, gantt, C4C diagram with nature language.",
 };
